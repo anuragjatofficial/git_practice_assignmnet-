8786cd2c0618f2e85de3e09a699da93311e9ab0a
anuragjatofficial <anurag.1101140@gmail.com>
2023-01-08 21:25:22 +0530

    check_palindrome_problem

---
+++ palindrom_problem.js
@@ -0,0 +1,11 @@
+var str = "naveen";
+var bag = "";
+for (let i = (str.length-1) ; i >=0; i--) {
+    bag += str[i];
+}
+if (bag==str) {
+    console.log(str,"is palindrome");
+}
+else{
+    console.log(str,"is not palindrome");
+}
